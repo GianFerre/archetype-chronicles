8e9e703c7e016036805528ff42b79943a765d5a3
Gian <gian.puse@oqulo.com>
2025-01-04 22:03:58 +0800

    Uploaded initial image for Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './index.css'
+import AC from "./assets/AC.png"
+import warrior from "./assets/warrior.png"
+import mage from "./assets/mage.png"
 
 function App() {
   return (
     <>
-      <h1 className="header-title">Archetype Chronicles</h1>
-      <img></img>
+      <div className='hero-text'>
+        <h1 className="header-title">Archetype Chronicles</h1>
+        <h3>Unlock the Secrets of Your Character.</h3>
+      </div>
+      <div className="hero-image">
+        <img src={warrior}></img>
+        <img src={AC}></img>
+        <img src={mage}></img>
+      </div>
     </>
   )
 }
